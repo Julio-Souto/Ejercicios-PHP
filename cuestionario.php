881cced2cd98f56fcb26859addf3fb42a30c94be
Julio-Souto <julio.souto.zas@gmail.com>
2023-04-20 16:18:01 +0200

    cuestionario PHP

---
+++ cuestionario.php
@@ -0,0 +1,116 @@
+<?php
+  extract($_POST);
+  $solucion1="";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <title>Document</title>
+  <style>
+    h3{
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+
+  <h3>Examen del Curso PHP</h3>
+  <h3>Primera semana</h3>
+  <h3>Fundamentos del lenguaje de programación PHP</h3>
+
+  <form method="post">
+    <p>1. ¿Cómo se abre y cierra un bloque de código PHP?</p>
+    <div class="radios">
+      <div><input type="radio" name="ejercicio1" id="resp11" value="resp11" required
+      <?php if(isset($ejercicio1) && $ejercicio1 == "resp11") echo "checked"; ?>>
+      <label for="resp11"><?php echo htmlspecialchars("<?php ?>"); ?></label></div>
+      <div><input type="radio" name="ejercicio1" id="resp12" value="resp12"
+      <?php if(isset($ejercicio1) && $ejercicio1 == "resp12") echo "checked"; ?>>
+      <label for="resp12"><?php echo htmlspecialchars("/**/"); ?></label></div>
+      <div><input type="radio" name="ejercicio1" id="resp13" value="resp13"
+      <?php if(isset($ejercicio1) && $ejercicio1 == "resp13") echo "checked"; ?>>
+      <label for="resp13"><?php echo htmlspecialchars("<script></script>"); ?></label></div>
+      <div><input type="radio" name="ejercicio1" id="resp14" value="resp14"
+      <?php if(isset($ejercicio1) && $ejercicio1 == "resp14") echo "checked"; ?>>
+      <label for="resp14"><?php echo htmlspecialchars("<body></body>"); ?></label></div>
+    </div>
+    <p><?=$solucion1;?></p>
+    <p>2. ¿Con qué símbolo se debe empezar el nombre de una variable en PHP?</p>
+    <div class="radios">
+      <div><input type="radio" name="ejercicio2" id="resp21" value="resp21" required
+      <?php if(isset($ejercicio2) && $ejercicio2 == "resp21") echo "checked"; ?>>
+      <label for="resp21"><?php echo htmlspecialchars("="); ?></label></div>
+      <div><input type="radio" name="ejercicio2" id="resp22" value="resp22"
+      <?php if(isset($ejercicio2) && $ejercicio2 == "resp22") echo "checked"; ?>>
+      <label for="resp22"><?php echo htmlspecialchars("=="); ?></label></div>
+      <div><input type="radio" name="ejercicio2" id="resp23" value="resp23"
+      <?php if(isset($ejercicio2) && $ejercicio2 == "resp23") echo "checked"; ?>>
+      <label for="resp23"><?php echo htmlspecialchars("//"); ?></label></div>
+      <div><input type="radio" name="ejercicio2" id="resp24" value="resp24"
+      <?php if(isset($ejercicio2) && $ejercicio2 == "resp24") echo "checked"; ?>>
+      <label for="resp24"><?php echo htmlspecialchars("$"); ?></label></div>
+    </div>
+    <p>3. ¿Con qué símbolo se debe envolver un valor numérico en una variable?</p>
+    <div class="radios">
+      <div><input type="radio" name="ejercicio3" id="resp31" value="resp31" required
+      <?php if(isset($ejercicio3) && $ejercicio3 == "resp31") echo "checked"; ?>>
+      <label for="resp31"><?php echo htmlspecialchars("\\\\"); ?></label></div>
+      <div><input type="radio" name="ejercicio3" id="resp32" value="resp32"
+      <?php if(isset($ejercicio3) && $ejercicio3 == "resp32") echo "checked"; ?>>
+      <label for="resp32"><?php echo htmlspecialchars("/**/"); ?></label></div>
+      <div><input type="radio" name="ejercicio3" id="resp33" value="resp33"
+      <?php if(isset($ejercicio3) && $ejercicio3 == "resp33") echo "checked"; ?>>
+      <label for="resp33"><?php echo htmlspecialchars("Ninguno, sólo se escribe el número"); ?></label></div>
+      <div><input type="radio" name="ejercicio3" id="resp34" value="resp34"
+      <?php if(isset($ejercicio3) && $ejercicio3 == "resp34") echo "checked"; ?>>
+      <label for="resp34"><?php echo htmlspecialchars("="); ?></label></div>
+    </div>
+    <p>4. ¿Cuál de las siguientes variables está declarada de forma correcta?</p>
+    <div class="radios">
+      <div><input type="radio" name="ejercicio4" id="resp41" value="resp41" required
+      <?php if(isset($ejercicio4) && $ejercicio4 == "resp41") echo "checked"; ?>>
+      <label for="resp41"><?php echo htmlspecialchars("edad = 25;"); ?></label></div>
+      <div><input type="radio" name="ejercicio4" id="resp42" value="resp42"
+      <?php if(isset($ejercicio4) && $ejercicio4 == "resp42") echo "checked"; ?>>
+      <label for="resp42"><?php echo htmlspecialchars("edad = “25”;"); ?></label></div>
+      <div><input type="radio" name="ejercicio4" id="resp43" value="resp43"
+      <?php if(isset($ejercicio4) && $ejercicio4 == "resp43") echo "checked"; ?>>
+      <label for="resp43"><?php echo htmlspecialchars("\$edad == 25;"); ?></label></div>
+      <div><input type="radio" name="ejercicio4" id="resp44" value="resp44"
+      <?php if(isset($ejercicio4) && $ejercicio4 == "resp44") echo "checked"; ?>>
+      <label for="resp44"><?php echo htmlspecialchars("\$edad = 25;"); ?></label></div>
+    </div>
+    <p>5. ¿Cuáles son los operadores relacionales?</p>
+    <div class="radios">
+      <div><input type="radio" name="ejercicio5" id="resp51" value="resp51" required 
+      <?php if(isset($ejercicio5) && $ejercicio5 == "resp51") echo "checked"; ?>>
+      <label for="resp51"><?php echo htmlspecialchars("+, -, *, /, %, ++, —"); ?></label></div>
+      <div><input type="radio" name="ejercicio5" id="resp52" value="resp52" 
+      <?php if(isset($ejercicio5) &&$ejercicio5 == "resp52") echo "checked"; ?>>
+      <label for="resp52"><?php echo htmlspecialchars("<, >, <=, >=, ==, !="); ?></label></div>
+      <div><input type="radio" name="ejercicio5" id="resp53" value="resp53" 
+      <?php if(isset($ejercicio5) &&$ejercicio5 == "resp53") echo "checked"; ?>>
+      <label for="resp53"><?php echo htmlspecialchars("&&, ||, and, or, !"); ?></label></div>
+      <div><input type="radio" name="ejercicio5" id="resp54" value="resp54" 
+      <?php if(isset($ejercicio5) &&$ejercicio5 == "resp54") echo "checked"; ?>>
+      <label for="resp54"><?php echo htmlspecialchars("$, &, //, /* */, { }"); ?></label></div>
+    </div>
+    <br>
+    <button name="enviar">Enviar</button>
+  </form>
+  <?php
+    if(isset($enviar)){
+      if($ejercicio1 == "resp11"){
+        $solucion1="Respuesta correcta";
+      }
+      else{
+        $solucion1="Respuesta incorrecta";
+      }
+    }
+  ?>
+</body>
+</html>
